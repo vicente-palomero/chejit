f165cf7c88d33dc48bd2b4dcdb2510c76912ede3
vicente palomero <vicente.palomero.lopez@gmail.com>
2020-09-22 09:00:37 +0200

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CheckIn />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CheckIn from './components/CheckIn'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CheckIn
   }
 }
 </script>
---
+++ src/components/CheckIn.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="checkIn">
+    <button>{{ buttonLabel }}</button>
+    <ul>
+      <li v-for="check in checks" v-bind:key="check.type">
+        {{ check.type }} - {{ new Date(check.time).toLocaleString('es-ES') }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CheckIn',
+  data: function () {
+    return {
+      checks: [
+        { type: 'in', time: Date.now() },
+        { type: 'out', time: Date.now() }
+      ]
+    }
+  },
+  computed: {
+    buttonLabel () {
+      return "my button"
+    }
+  }
+}
+</script>
